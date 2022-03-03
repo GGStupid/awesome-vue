403dbc0fb07d4994e572756f3c1da4888a663c57
GGStupid <610383057@qq.com>
2022-03-03 22:28:51 +0800

    feat:init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input v-model="msg" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "@vue/runtime-dom";
+
+const msg = ref<string>("Hello World");
+</script>
